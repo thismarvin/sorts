e74c240009dd3fc0db7f30113085dc66d776e0cc
thismarvin <bend_@outlook.com>
2020-01-23 14:10:24 -0600

    all the graphs are in + better html generation

--- src/main.mjs
+++ src/main.mjs
@@ -8,5 +8,32 @@ import {
     CocktailSorter,
 } from "./modules/visualizers.mjs"
 
-const visualizer = new CocktailSorter("visualizer");
-visualizer.begin();
+const sortTypes = ["insertion", "selection", "quick", "merge", "bubble", "cocktail", "bogo"];
+const rootDiv = document.getElementById("root");
+
+let div;
+for (let sortType of sortTypes){
+    div = document.createElement("div");
+    rootDiv.appendChild(div);  
+    div.insertAdjacentHTML(
+        "afterend",
+        `<div class="visualization">
+            <h2>${sortType} sort</h2>
+            <div class="visualizer" id="${sortType}"></div>
+        </div>`
+    );
+}
+
+const visualizers = [
+    new InsertionSorter("insertion"),
+    new SelectionSorter("selection"),
+    new QuickSorter("quick"),
+    new BubbleSorter("bubble"),
+    new MergeSorter("merge"),
+    new BogoSorter("bogo"),
+    new CocktailSorter("cocktail"),
+];
+
+for (let v of visualizers) {
+    v.begin();
+}
